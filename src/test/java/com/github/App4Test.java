aaca7c9f3e0d1f02193931e04b5ce61f4623dd16
Tim Jacomb <timjacomb1+github@gmail.com>
2020-10-10 11:41:43 +0100

    More than 10 failing

---
+++ src/test/java/com/github/App4Test.java
@@ -0,0 +1,90 @@
+package com.github;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+public class App4Test {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void attachment() throws IOException {
+        Path file = Paths.get("hello.txt");
+        List<String> linesInMemory = Collections.singletonList("HEY HEY");
+        Files.write(file, linesInMemory, StandardCharsets.UTF_8);
+
+        System.out.println("[[ATTACHMENT|" + file.toAbsolutePath()  + "]]");
+        
+        assertThat(false, is(true));
+    }
+
+    @Test
+    @Ignore
+    public void attachment2() throws IOException {
+        Path file = Paths.get("hello.txt");
+        List<String> linesInMemory = Collections.singletonList("HEY HEY");
+        Files.write(file, linesInMemory, StandardCharsets.UTF_8);
+
+        System.out.println("[[ATTACHMENT|" + file.toAbsolutePath()  + "]]");
+
+        assertThat(false, is(true));
+    }
+
+    @Test
+    public void attachment3() throws IOException {
+        Path file = Paths.get("hello.txt");
+        List<String> linesInMemory = Collections.singletonList("HEY HEY");
+        Files.write(file, linesInMemory, StandardCharsets.UTF_8);
+
+        System.out.println("[[ATTACHMENT|" + file.toAbsolutePath()  + "]]");
+
+        assertThat(false, is(true));
+    }
+
+    @Test
+    public void attachment4() throws IOException {
+        Path file = Paths.get("hello.txt");
+        List<String> linesInMemory = Collections.singletonList("HEY HEY");
+        Files.write(file, linesInMemory, StandardCharsets.UTF_8);
+
+        System.out.println("[[ATTACHMENT|" + file.toAbsolutePath()  + "]]");
+
+        assertThat(false, is(true));
+    }
+
+    @Test
+    public void attachment5() throws IOException {
+        Path file = Paths.get("hello.txt");
+        List<String> linesInMemory = Collections.singletonList("HEY HEY");
+        Files.write(file, linesInMemory, StandardCharsets.UTF_8);
+
+        System.out.println("[[ATTACHMENT|" + file.toAbsolutePath()  + "]]");
+
+        assertThat(false, is(true));
+    }
+
+    @Test
+    @Ignore
+    public void attachment6() throws IOException {
+        Path file = Paths.get("hello.txt");
+        List<String> linesInMemory = Collections.singletonList("HEY HEY");
+        Files.write(file, linesInMemory, StandardCharsets.UTF_8);
+
+        System.out.println("[[ATTACHMENT|" + file.toAbsolutePath()  + "]]");
+
+        assertThat(false, is(true));
+    }
+}
